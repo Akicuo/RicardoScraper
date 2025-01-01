2f21aa71a8b82b9a14208cc1529f111808f08b40
Akic <user.of.replit@gmail.com>
2025-01-01 15:27:46 +0100

    wda

---
+++ main.py
@@ -0,0 +1,134 @@
+import requests 
+from bs4 import BeautifulSoup
+import random
+url = "https://www.ricardo.ch/"
+def AmountOfOfferPages(name: str) -> int:
+    global url
+    pages = 0
+    page = 1
+    while True:
+        response = requests.get(url=url+f"de/shop/{name}/offers/?page={page}")
+        if response.status_code != 200:
+            break
+        pages += 1
+        page += 1
+    return pages
+
+def ShopOffers(name:str, page:int=1, exclude_owi:bool=False, links_only:bool=False):
+    global url
+    v_results = []
+
+    if page < 1:
+        raise ValueError("page parameter has to be 1 or over")
+    
+    html = requests.get(url=url+F"de/shop/{name}/offers/?page={str(page)}")
+    if html.status_code == 404:
+        return ["None found"]
+    s = BeautifulSoup(html.content, 'html.parser')
+    results = s.find_all('a', class_="style_link__ewXtk")
+    if links_only:
+        for result in results:
+            v_results.append(result.get('href'))
+    else:
+        for result in results:
+            text_elements = result.stripped_strings
+            img_tag = result.find('img', class_="MuiBox-root mui-htynqc")
+            img = img_tag.get("src") if img_tag else None  # Handle None case
+            if exclude_owi and img is None:
+                continue
+            separated_texts = "|".join(text_elements).split("|")
+            if len(separated_texts) == 3:
+                v_results.append({  "shop": name,
+                                    "image": img,
+                                    "title": separated_texts[0],
+                                    "price": float(separated_texts[1]),
+                                    "link": "https://www.ricardo.ch"+result.get('href')})
+            elif len(separated_texts) == 4:
+                v_results.append({  "shop": name,
+                                    "image": img,
+                                    "title": separated_texts[0],
+                                    "extra": separated_texts[1],
+                                    "price": float(str(separated_texts[2]).replace("'", "")),
+                                    "link": "https://www.ricardo.ch"+result.get('href')})
+
+    return v_results
+
+
+def ShopRatings(name:str, names_only=False):
+    global url
+    html = requests.get(url=url+F"de/shop/{name}/ratings/")
+    s = BeautifulSoup(html.content, 'html.parser')
+    results = s.find_all('div', class_="MuiBox-root mui-x1sij0")
+    v_results=None
+    if names_only:
+        v_results = []
+        print("searching for names only")
+        for result in results:
+            v_results.append("|".join(result.stripped_strings).split("|")[0])
+        
+    else:
+        v_results = {"polarity": {}, "ratings": []}
+
+        # get polartiy (positive neutral negative score)
+        results_v2 = s.find_all('div', class_="mui-1qtkas9")
+        for result in results_v2:
+            separated_texts = "|".join(result.stripped_strings).split("|")
+            v_results["polarity"][str(separated_texts[0].lower()+"e").replace("neutrale", "neutrals")] = int(separated_texts[1])
+
+
+
+        # get ratings 
+        for result in results:
+            separated_texts = "|".join(result.stripped_strings).split("|")
+            v_results["ratings"].append({"type": separated_texts[3].lower()+"e",
+                              "user": separated_texts[0],
+                              "article-nr": int(separated_texts[2].replace("Art.-Nr. ", "")),
+                              "content": separated_texts[4]})
+    return v_results
+
+
+
+
+class Crawler:
+    def ContinousShopFinder(self, start_shop: str):
+        current_shop = start_shop
+        shops_found = set()
+        shops_crawled = set()
+        while True:
+            try:
+                raters = ShopRatings(current_shop, True)
+                shops_found.update(raters)
+                shops_crawled.add(current_shop)
+
+                not_yet_crawled = shops_found - shops_crawled
+                if not_yet_crawled:
+                    current_shop = random.choice(list(not_yet_crawled))
+
+                chunk = {"users": len(raters),
+                         "total_shops": len(shops_found),
+                         "total_shops_c": len(shops_crawled),
+                         "not_crawled": len(not_yet_crawled)
+                         }    
+                yield chunk  # Yield the chunk dictionary instead of raters
+            except Exception as e:
+                print(f"An error occurred: {e}")
+                break
+
+"""
+Example use of the continous crawler
+csf = Crawler().ContinousShopFinder("Senn24") # Recommend to use someone that has alot of raters
+for chunk in csf:                                                           
+    print(f"Found: {chunk['users']}")
+    print(f"Shops Total: {chunk['total_shops']}")
+    print(f"Shops Crawled: {chunk['total_shops_c']}")
+    print(f"Not yet crawled: {chunk['not_crawled']}")
+    print(f"Progress: {chunk['total_shops_c'] / chunk['total_shops']* 100:.2f}%")
+    print("\n\n")
+
+Parameters explained:
+exclude_owi = Exclude any offer/s without image
+-> if turned on will only append offers to the list that have a thumbnail
+
+name
+-> name of the shop / seller
+"""
---
+++ readme.md
@@ -0,0 +1,56 @@
+# Ricardo Web Crawler
+
+This Python script provides a set of tools to crawl and extract data from the Ricardo.ch website. It includes functions to retrieve shop offers, ratings, and a continuous shop finder that explores related shops.
+
+## Features
+
+- **AmountOfOfferPages**: Determines the number of offer pages available for a specific shop.
+- **ShopOffers**: Retrieves offers from a specific shop, with options to exclude offers without images and return only links.
+- **ShopRatings**: Fetches ratings for a specific shop, with an option to return only the names of the raters.
+- **ContinousShopFinder**: A generator that continuously finds and crawls related shops based on raters.
+
+## Installation
+
+To use this script, you need to have Python installed along with the following libraries:
+
+```bash
+pip install requests beautifulsoup4
+```
+## Usage
+
+### AmountOfOfferPages | Get the amount of pages a shop/seller has
+```
+pages = AmountOfOfferPages("Senn24")
+print(f"Number of offer pages: {pages}")
+```
+
+### ShopOffers 
+```
+offers = ShopOffers("Senn24", page=1, exclude_owi=True, links_only=False)
+for offer in offers:
+    print(offer)
+```
+### ShopRatings
+```
+ratings = ShopRatings("Senn24", names_only=False)
+print(ratings)
+```
+
+### ContinousShopFinder
+```
+csf = Crawler().ContinousShopFinder("Senn24")
+for chunk in csf:
+    print(f"Found: {chunk['users']}")
+    print(f"Shops Total: {chunk['total_shops']}")
+    print(f"Shops Crawled: {chunk['total_shops_c']}")
+    print(f"Not yet crawled: {chunk['not_crawled']}")
+    print(f"Progress: {chunk['total_shops_c'] / chunk['total_shops'] * 100:.2f}%")
+    print("\n\n")
+```
+## Parameters
+
+- **exclude_owi**: Exclude any offers without images.
+- **name**: Name of the shop/seller.
+- **page**: Page number to retrieve offers from.
+- **links_only**: Return only the links of the offers.
+- **names_only**: Return only the names of the raters.
