5195d4ba2556324e792f9ba3baa14c7a90da400b
Akic <user.of.replit@gmail.com>
2025-01-27 23:49:26 +0100

    awd

--- scraper.py
+++ scraper.py
@@ -87,11 +87,6 @@ def product(url: str):
         }
 
 
-e = product("https://www.ricardo.ch/de/a/imac-(retina-5k-27-zoll-ende-2015)-1278553784/")
-print(e)
-with open("result.json", "w") as vb:
-    json.dump(e, vb, indent=4)
-
 
 
 def AmountOfOfferPages(name: str) -> list:
